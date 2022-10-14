1967e9dff3855016f52c1679c35c2225fb45e454
Thorsten Lorenz <thlorenz@gmx.de>
2022-10-14 14:11:30 -0500

    feat: optional instruction acccounts defaulting to program_id (#40)
    * ix: parsing out instruction strategy attribute (default_optional_accounts)
    
    * macro: adding default_optional_accounts attribute
    
    * macro: store strategies in hashset
    
    - only one instance of a particular strategy makes sense per instruction
    
    * idl: integrate strategy as boolean in instruction and added tests

--- shank-macro-impl/src/instruction/mod.rs
+++ shank-macro-impl/src/instruction/mod.rs
@@ -1,12 +1,16 @@
 mod account_attrs;
 mod extract_instructions;
 mod instruction;
+mod strategy_attrs;
 
 pub use account_attrs::*;
 pub use extract_instructions::*;
 pub use instruction::*;
+pub use strategy_attrs::*;
 
 #[cfg(test)]
 mod account_attrs_test;
 #[cfg(test)]
 mod instruction_test;
+#[cfg(test)]
+mod strategy_attrs_test;
