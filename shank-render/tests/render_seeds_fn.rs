d2df2499afe74ee7bdb4732870106acbc1c1a29e
Thorsten Lorenz <thlorenz@gmx.de>
2022-11-15 15:25:31 +0200

    chore: render tests for candy guard + pretty printing rendered tokens

--- shank-render/tests/render_seeds_fn.rs
+++ shank-render/tests/render_seeds_fn.rs
@@ -27,12 +27,20 @@ fn render_seeds(code: TokenStream) -> TokenStream {
         .unwrap()
 }
 
+fn pretty_print(code: TokenStream) -> String {
+    let syn_tree = syn::parse_file(code.to_string().as_str()).unwrap();
+    prettyplease::unparse(&syn_tree)
+}
+
+#[allow(dead_code)]
+fn render_and_dump(code: &TokenStream) {
+    let rendered = render_seeds(code.clone());
+    eprintln!("{}", pretty_print(rendered));
+}
+
 fn assert_rendered_seeds_fn(code: TokenStream, expected: TokenStream) {
     let rendered = render_seeds(code);
-    assert_eq!(
-        rendered.to_string().replace(" 'a", "'a"),
-        expected.to_string().replace(" 'a", "'a")
-    );
+    assert_eq!(pretty_print(rendered), pretty_print(expected));
 }
 
 #[test]
@@ -62,6 +70,79 @@ fn literal_pubkeys_and_u8_byte() {
                     program_id.as_ref(),
                     some_pubkey.as_ref(),
                     some_byte
+                ]
+            }
+        },
+    );
+}
+
+#[test]
+fn candy_guard_edition_marker_seeds() {
+    let code = quote! {
+        #[derive(ShankAccount)]
+        #[seeds(
+            prefix("Prefix", str),
+            program_id,
+            master_edition_mint_info("Master Edition Mint Info", AccountInfo),
+            edition("Edition", str),
+            edition_marker_number("Edition Marker Number", String),
+        )]
+        struct CandyGuardEditionMarker {
+            count: u8,
+        }
+    };
+
+    assert_rendered_seeds_fn(
+        code,
+        quote! {
+            pub fn account_seeds<'a>(
+                prefix: &'a str,
+                program_id: &'a ::solana_program::pubkey::Pubkey,
+                master_edition_mint_info: &'a ::solana_program::account_info::AccountInfo,
+                edition: &'a str,
+                edition_marker_number: &'a String,
+            ) -> [&'a [u8]; 5usize] {
+                [
+                    prefix.as_bytes(),
+                    program_id.as_ref(),
+                    master_edition_mint_info.as_ref(),
+                    edition.as_bytes(),
+                    edition_marker_number.as_bytes(),
+                ]
+            }
+        },
+    );
+}
+
+#[test]
+fn candy_candy_guard_mint_limit_seeds() {
+    let code = quote! {
+        #[derive(ShankAccount)]
+        #[seeds(
+            id("Guard Id", u8),
+            user("The User Pubkey"),
+            candy_guard_key("Candy Guard Key", Pubkey),
+            candy_machine_key("Candy Machine Key"),
+        )]
+        struct CandyGuardMintLimitSeeds {
+            count: u8,
+        }
+    };
+
+    assert_rendered_seeds_fn(
+        code,
+        quote! {
+            pub fn account_seeds<'a>(
+                id: &'a [u8; 1usize],
+                user: &'a ::solana_program::pubkey::Pubkey,
+                candy_guard_key: &'a ::solana_program::pubkey::Pubkey,
+                candy_machine_key: &'a ::solana_program::pubkey::Pubkey,
+            ) -> [&'a [u8]; 4usize] {
+                [
+                    id,
+                    user.as_ref(),
+                    candy_guard_key.as_ref(),
+                    candy_machine_key.as_ref(),
                 ]
             }
         },
