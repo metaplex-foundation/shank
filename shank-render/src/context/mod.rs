e02efc7c08b133c6fb34c2878a4391b787eb781d
febo <febo@metaplex.com>
2023-06-20 10:33:46 +0100

    Add context renderer

---
+++ shank-render/src/context/mod.rs
@@ -0,0 +1,45 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use shank_macro_impl::instruction::Instruction;
+use shank_macro_impl::syn::Result as ParseResult;
+
+mod render_context;
+use self::render_context::generate_context;
+
+pub fn render_contexts_impl(
+    instruction: &Instruction,
+) -> ParseResult<TokenStream> {
+    let contexts = instruction
+        .variants
+        .iter()
+        .map(generate_context)
+        .collect::<Vec<TokenStream>>();
+
+    Ok(quote! {
+    pub struct Context<'a, T> {
+        pub accounts: T,
+        pub remaining_accounts: Vec<&'a solana_program::account_info::AccountInfo<'a>>,
+    }
+
+    impl<'a, T> Context<'a, T> {
+        /// Convenience function for accessing the next item in an [`AccountInfo`]
+        /// iterator and validating whether the account is present or not.
+        ///
+        /// This relies on the client setting the `crate::ID` as the pubkey for
+        /// accounts that are not set, which effectively allows us to use positional
+        /// optional accounts.
+        pub fn next_optional_account_info<'b, 'c, I: Iterator<Item = &'b solana_program::account_info::AccountInfo<'c>>>(
+            iter: &mut I,
+        ) -> Result<Option<I::Item>, solana_program::program_error::ProgramError> {
+            let account_info = iter.next().ok_or(solana_program::program_error::ProgramError::NotEnoughAccountKeys)?;
+
+            Ok(if *account_info.key == crate::ID {
+                None
+            } else {
+                Some(account_info)
+            })
+        }
+    }
+
+    #(#contexts)* })
+}
---
+++ shank-render/src/context/render_context.rs
@@ -0,0 +1,68 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use shank_macro_impl::instruction::InstructionVariant;
+
+pub(crate) fn generate_context(variant: &InstructionVariant) -> TokenStream {
+    // accounts names
+    let fields = variant.accounts.iter().map(|account| {
+        let account_name = syn::parse_str::<syn::Ident>(
+            format!("{}_info", &account.name).as_str(),
+        )
+        .unwrap();
+        quote! { #account_name }
+    });
+
+    // accounts fields
+    let struct_fields = variant.accounts.iter().map(|account| {
+            let account_name = syn::parse_str::<syn::Ident>(format!("{}_info", &account.name).as_str()).unwrap();
+            if account.optional {
+                quote! {
+                    pub #account_name: Option<&'a solana_program::account_info::AccountInfo<'a>>
+                }
+            } else {
+                quote! {
+                    pub #account_name:&'a solana_program::account_info::AccountInfo<'a>
+                }
+            }
+        });
+
+    // accounts initialization for the impl block
+    let impl_fields = variant.accounts.iter().map(|account| {
+            let account_name = syn::parse_str::<syn::Ident>(format!("{}_info", &account.name).as_str()).unwrap();
+            if account.optional {
+                quote! {
+                    let #account_name = Self::next_optional_account_info(account_info_iter)?;
+                }
+            } else {
+                quote! {
+                    let #account_name = solana_program::account_info::next_account_info(account_info_iter)?;
+                }
+            }
+        });
+
+    let name = &variant.ident;
+
+    quote! {
+        pub struct #name<'a> {
+            #(#struct_fields,)*
+        }
+        impl<'a> #name<'a> {
+            pub fn to_context(
+                accounts: &'a [solana_program::account_info::AccountInfo<'a>]
+            ) -> Result<Context<'a, Self>, solana_program::sysvar::slot_history::ProgramError> {
+                let account_info_iter = &mut accounts.iter();
+
+                #(#impl_fields)*
+
+                let accounts = Self {
+                    #(#fields,)*
+                };
+
+                Ok(Context {
+                    accounts,
+                    remaining_accounts: Vec::<&'a solana_program::account_info::AccountInfo<'a>>::from_iter(account_info_iter),
+                })
+            }
+        }
+    }
+}
