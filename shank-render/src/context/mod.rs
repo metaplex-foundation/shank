8b0f2fe5fb628d41e5dd88a341ba51ba641a58a2
Blockiosaurus <blockiosaurus@gmail.com>
2025-01-18 15:06:33 -0500

    Prelim pinocchio support

--- shank-render/src/context/mod.rs
+++ shank-render/src/context/mod.rs
@@ -15,13 +15,19 @@ pub fn render_contexts_impl(
         .map(generate_context)
         .collect::<Vec<TokenStream>>();
 
+    let account_info_type = if cfg!(feature = "pinocchio") {
+        quote! { pinocchio::account_info::AccountInfo }
+    } else {
+        quote! { solana_program::account_info::AccountInfo<'a> }
+    };
+
     Ok(quote! {
         pub mod accounts {
             use super::*;
 
             pub struct Context<'a, T> {
                 pub accounts: T,
-                pub remaining_accounts: &'a [solana_program::account_info::AccountInfo<'a>],
+                pub remaining_accounts: &'a [#account_info_type],
             }
 
             #(#contexts)*
